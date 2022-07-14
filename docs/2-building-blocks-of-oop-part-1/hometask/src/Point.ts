3a5a2461e30e8c35996d0560da6153de8cd26da8
volodymyr <volodymyr.kartaiev@kingfisher.com>
2022-07-14 08:59:43 +0300

    first home-task

--- docs/2-building-blocks-of-oop-part-1/hometask/src/Point.ts
+++ docs/2-building-blocks-of-oop-part-1/hometask/src/Point.ts
@@ -1,1 +1,31 @@
-export class Point {}
+import {getDistance} from "./utils";
+
+export class Point {
+   public x: number;
+   public y: number;
+
+    constructor();
+    constructor(x: number, y: number) {
+        this.x = x ?? 0;
+        this.y = y ?? 0;
+    }
+
+   public toString(): string {
+        return `(${this.x}, ${this.y})`;
+    }
+
+   public distance(): number;
+   public distance(other: Point): number;
+   public distance(x: number, y: number): number {
+        if (other) {
+            return getDistance(this.x - other.x, this.y - other.y);
+        }
+
+
+        if (x && y) {
+            return getDistance(this.x - x, this.y - y);
+        }
+
+        return getDistance(this.x, this.y);
+    }
+}
--- docs/2-building-blocks-of-oop-part-1/hometask/src/Shape.ts
+++ docs/2-building-blocks-of-oop-part-1/hometask/src/Shape.ts
@@ -1,3 +1,26 @@
+import { Point } from "./Point";
+
+const defaultPoints = [new Point(2, 4), new Point(4, 6), new Point()]
+
 export abstract class Shape {
+    protected color: string
+    protected filled: boolean
+    points: Point[]
+
+    constructor();
+    constructor(color: string, filled: boolean, points: Point[]) {
+        this.color = color ?? 'green';
+        this.filled = filled ?? true;
+        this.points = points ?? defaultPoints;
+    }
+
+    public toString(): string {
+        return `A shape with color ${this.color} and ${this.filled ? 'filled' : 'not filled'}. Points: ${this.points.map(p => p.toString()).join(', ')}`;
+    }
+
+    public getPerimeter(): number {
+        return this.points.reduce((acc, p) => acc + p.distance(), 0);
+    }
+
     abstract getType(): string;
 }
